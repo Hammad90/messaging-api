7ca2c4e3c5ce072edcae5abd4743f58532742728
Hammad90 <moh.hammad.90@gmail.com>
2024-09-15 15:19:45 +0900

    fix: create BaseTestCase with the common setup for all tests

--- messaging_app/user/tests.py
+++ messaging_app/user/tests.py
@@ -1,37 +1,18 @@
+from core.tests import BaseTestCase
 from django.urls import reverse
-from rest_framework.status import HTTP_200_OK, HTTP_201_CREATED
-from rest_framework.test import APIClient, APITestCase
+from rest_framework.status import HTTP_201_CREATED
 
 from .models import Users
 from .views import UserViewSet
 
 
-class UserViewSetTestCase(APITestCase):
+class UserViewSetTestCase(BaseTestCase):
 
     def setUp(self):
-        self.user = Users.objects.create(
-            username="testuser1",
-            password="testpassword",
-            email="test@example.com",
-            first_name="test",
-            last_name="last",
-        )
-        self.user.set_password("testpassword")
-        self.user.save()
-        self.client = APIClient()
+        super().setUp()
         self.user_list_url = reverse("user-list")
-        print("%s", self.user_list_url)
 
-    def authenticate(self):
-        url = reverse("token_obtain_pair")
-        response = self.client.post(
-            url, {"username": "testuser1", "password": "testpassword"}, format="json"
-        )
-        self.assertEqual(response.status_code, HTTP_200_OK)
-        token = response.data["access"]
-        self.client.credentials(HTTP_AUTHORIZATION=f"Bearer {token}")
-
-    def test_create_non_authenticated(self):
+    def test_create_user_without_authentication(self):
         data = {
             "username": "newuser",
             "password": "newpassword",
@@ -41,12 +22,12 @@ class UserViewSetTestCase(APITestCase):
         }
         response = self.client.post(self.user_list_url, data, format="json")
         self.assertEqual(response.status_code, HTTP_201_CREATED)
-        self.assertEqual(Users.objects.count(), 2)
+        self.assertEqual(Users.objects.count(), 3)
         self.assertEqual(
             Users.objects.get(username="newuser").email, "newemail@example.com"
         )
 
-    def test_create_authenticated(self):
+    def test_create_user_with_authentication(self):
         data = {
             "username": "newuser",
             "password": "newpassword",
@@ -58,7 +39,7 @@ class UserViewSetTestCase(APITestCase):
         UserViewSet.permission_classes = []
         response = self.client.post(self.user_list_url, data, format="json")
         self.assertEqual(response.status_code, HTTP_201_CREATED)
-        self.assertEqual(Users.objects.count(), 2)
+        self.assertEqual(Users.objects.count(), 3)
         self.assertEqual(
             Users.objects.get(username="newuser").email, "newemail@example.com"
         )
